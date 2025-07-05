d8513d11b298ed76ff00ced267220a5581d84b7f
de-sam <samsoncoded@gmail.com>
2025-07-05 18:51:00 +0100

    task completed

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -6,10 +6,11 @@ SELECT
     COUNT(b.booking_id) AS total_bookings
 FROM users u
 LEFT JOIN bookings b ON u.user_id = b.user_id
-GROUP BY u.user_id, u.first_name, u.last_name;
+GROUP BY u.user_id, u.first_name, u.last_name
+ORDER BY total_bookings DESC;
 
 
--- 2. Rank properties based on total number of bookings using RANK()
+-- 2a. Use RANK() to rank properties by total number of bookings
 SELECT 
     p.property_id,
     p.name AS property_name,
@@ -17,4 +18,17 @@ SELECT
     RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank
 FROM properties p
 LEFT JOIN bookings b ON p.property_id = b.property_id
-GROUP BY p.property_id, p.name;
+GROUP BY p.property_id, p.name
+ORDER BY booking_rank;
+
+
+-- 2b. Use ROW_NUMBER() to assign a unique row number to each property ordered by bookings
+SELECT 
+    p.property_id,
+    p.name AS property_name,
+    COUNT(b.booking_id) AS total_bookings,
+    ROW_NUMBER() OVER (ORDER BY COUNT(b.booking_id) DESC) AS row_num
+FROM properties p
+LEFT JOIN bookings b ON p.property_id = b.property_id
+GROUP BY p.property_id, p.name
+ORDER BY row_num;
